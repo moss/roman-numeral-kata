21f87ac1e25a7c70ed4b0e1e344688f12f672d52
Moss Collum <moss.collum@gmail.com>
2013-03-07 16:05:22 -0500

    refactor: exceptions to handle errors

--- src/net/m14m/romannumeralkata/RomanNumeralParser.java
+++ src/net/m14m/romannumeralkata/RomanNumeralParser.java
@@ -10,12 +10,19 @@ public class RomanNumeralParser {
     }};
 
     int translate(String input) {
+        try {
+            return translateUnsafe(input);
+        } catch (NullPointerException e) {
+            return -1;
+        }
+    }
+
+    private int translateUnsafe(String input) {
         int value = 0;
         char[] chars = input.toLowerCase().toCharArray();
         int prevValue = 0;
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            if (!valuesOfLetters.containsKey(c)) return -1;
             int charValue = valuesOfLetters.get(c);
             if (charValue > prevValue) value -= (prevValue * 2);
             value += charValue;
