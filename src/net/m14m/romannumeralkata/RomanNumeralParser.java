26a595b8a7c9920e2daf206ce20670c9371b9bfd
Moss Collum <moss.collum@gmail.com>
2013-03-07 15:39:44 -0500

    Pass - lowercase.

--- src/net/m14m/romannumeralkata/RomanNumeralParser.java
+++ src/net/m14m/romannumeralkata/RomanNumeralParser.java
@@ -3,9 +3,9 @@ package net.m14m.romannumeralkata;
 public class RomanNumeralParser {
     int translate(String input) {
         int value = 0;
-        for (char c : input.toCharArray()) {
-            if (c == 'I') value += 1;
-            if (c == 'V') value += 5;
+        for (char c : input.toLowerCase().toCharArray()) {
+            if (c == 'i') value += 1;
+            if (c == 'v') value += 5;
         }
         return value;
     }
