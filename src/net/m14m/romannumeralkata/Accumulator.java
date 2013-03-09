e1d51756ea06fa73a453801e86abb457e8ef906b
Moss Collum <moss.collum@gmail.com>
2013-03-09 12:30:48 -0500

    pass: XIV (trying again)

--- src/net/m14m/romannumeralkata/Accumulator.java
+++ src/net/m14m/romannumeralkata/Accumulator.java
@@ -4,12 +4,17 @@ import static net.m14m.romannumeralkata.RomanNumeralSymbol.nullSymbol;
 
 public class Accumulator {
     private int value = 0;
+    private int groupValue = 0;
     private RomanNumeralSymbol latestSymbol = nullSymbol();
 
     public void push(RomanNumeralSymbol symbol) {
-        value += latestSymbol.value;
+        groupValue += latestSymbol.value;
         if (symbol.outranks(latestSymbol)) {
-            value = -value;
+            value -= groupValue;
+            groupValue = 0;
+        } else if (latestSymbol.outranks(symbol)) {
+            value += groupValue;
+            groupValue = 0;
         }
         latestSymbol = symbol;
     }
