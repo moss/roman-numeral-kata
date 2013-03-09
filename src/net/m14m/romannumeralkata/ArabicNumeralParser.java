d732c809da17ac2eaf7317359a1f9186aeb4d964
Moss Collum <moss.collum@gmail.com>
2013-03-09 13:24:18 -0500

    pass: V, not too ambitious really

--- src/net/m14m/romannumeralkata/ArabicNumeralParser.java
+++ src/net/m14m/romannumeralkata/ArabicNumeralParser.java
@@ -4,6 +4,10 @@ public class ArabicNumeralParser {
     public String translate(Integer value) {
         StringBuilder result = new StringBuilder();
         for (int i=0; i< value; i++) {
+            if (value == 5) {
+                result.append("V");
+                break;
+            }
             result.append("I");
         }
         return result.toString();
