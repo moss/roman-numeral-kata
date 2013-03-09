2ea2aae90fe806fbd9813e8293e2ee07ffc15c91
Moss Collum <moss.collum@gmail.com>
2013-03-09 13:38:05 -0500

    refactor: pull out a class to represent the concept of a roman numeral symbol in arabic numeral parser land

--- src/net/m14m/romannumeralkata/ArabicNumeralParser.java
+++ src/net/m14m/romannumeralkata/ArabicNumeralParser.java
@@ -1,22 +1,36 @@
 package net.m14m.romannumeralkata;
 
 public class ArabicNumeralParser {
+    private static final Symbol I = new Symbol(1, "I");
+    private static final Symbol V = new Symbol(5, "V");
+    private static final Symbol X = new Symbol(10, "X");
+
     public String translate(Integer value) {
         if (value <= 0) return "ERROR";
         StringBuilder result = new StringBuilder();
         int remainingValue = value;
         while (remainingValue > 0) {
-            if (remainingValue >= 10) {
-                result.append("X");
-                remainingValue -= 10;
-            } else if (remainingValue >= 5) {
-                result.append("V");
-                remainingValue -= 5;
-            } else if (remainingValue >= 1) {
-                result.append("I");
-                remainingValue -= 1;
+            if (remainingValue >= X.value) {
+                result.append(X.symbol);
+                remainingValue -= X.value;
+            } else if (remainingValue >= V.value) {
+                result.append(V.symbol);
+                remainingValue -= V.value;
+            } else if (remainingValue >= I.value) {
+                result.append(I.symbol);
+                remainingValue -= I.value;
             }
         }
         return result.toString();
+    }
+
+    private static class Symbol {
+        private int value;
+        private String symbol;
+
+        private Symbol(int value, String symbol) {
+            this.value = value;
+            this.symbol = symbol;
+        }
     }
 }
