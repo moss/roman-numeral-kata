a0e3505629048643b020d75df2aab061fa8dce8e
Moss Collum <moss.collum@gmail.com>
2013-03-09 13:31:25 -0500

    pass: 11

--- src/net/m14m/romannumeralkata/ArabicNumeralParser.java
+++ src/net/m14m/romannumeralkata/ArabicNumeralParser.java
@@ -5,7 +5,10 @@ public class ArabicNumeralParser {
         StringBuilder result = new StringBuilder();
         int remainingValue = value;
         while (remainingValue > 0) {
-            if (remainingValue >= 5) {
+            if (remainingValue >= 10) {
+                result.append("X");
+                remainingValue -= 10;
+            } else if (remainingValue >= 5) {
                 result.append("V");
                 remainingValue -= 5;
             } else {
