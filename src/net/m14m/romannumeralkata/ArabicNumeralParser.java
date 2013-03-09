6f88806adb5c1214ef0bebe53b778d06072fdf2a
Moss Collum <moss.collum@gmail.com>
2013-03-09 13:27:16 -0500

    refactor: rename i -> remainingValue

--- src/net/m14m/romannumeralkata/ArabicNumeralParser.java
+++ src/net/m14m/romannumeralkata/ArabicNumeralParser.java
@@ -3,14 +3,14 @@ package net.m14m.romannumeralkata;
 public class ArabicNumeralParser {
     public String translate(Integer value) {
         StringBuilder result = new StringBuilder();
-        int i=value;
-        while (i > 0) {
+        int remainingValue = value;
+        while (remainingValue > 0) {
             if (value == 5) {
                 result.append("V");
                 break;
             }
             result.append("I");
-            i--;
+            remainingValue--;
         }
         return result.toString();
     }
