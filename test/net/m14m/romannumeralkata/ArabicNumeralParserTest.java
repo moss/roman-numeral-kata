ea1f5de9451077e0882e23d5d10c363786e2e2fc
Moss Collum <moss.collum@gmail.com>
2013-03-29 18:21:57 -0400

    refactor: test cleanup

--- test/net/m14m/romannumeralkata/ArabicNumeralParserTest.java
+++ test/net/m14m/romannumeralkata/ArabicNumeralParserTest.java
@@ -5,9 +5,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ArabicNumeralParserTest {
+    private ArabicNumeralParser parser = new ArabicNumeralParser();
+
     @Test public void shouldParseNumbers() {
-        assertEquals(4, new ArabicNumeralParser().parseValue("4"));
-        assertEquals(15, new ArabicNumeralParser().parseValue("15"));
-        assertEquals(2537, new ArabicNumeralParser().parseValue("2537"));
+        assertEquals(4, parser.parseValue("4"));
+        assertEquals(15, parser.parseValue("15"));
+        assertEquals(2537, parser.parseValue("2537"));
     }
 }
