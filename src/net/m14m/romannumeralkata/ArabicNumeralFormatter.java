0351a4263eea5e31c88c8ee72ba1bd17e167e768
Moss Collum <moss.collum@gmail.com>
2013-03-29 18:26:42 -0400

    refactor: ArabicNumeralFormatter implement Formatter

--- src/net/m14m/romannumeralkata/ArabicNumeralFormatter.java
+++ src/net/m14m/romannumeralkata/ArabicNumeralFormatter.java
@@ -1,6 +1,6 @@
 package net.m14m.romannumeralkata;
 
-public class ArabicNumeralFormatter {
+public class ArabicNumeralFormatter implements Formatter {
     public String format(int value) {
         return String.valueOf(value);
     }
