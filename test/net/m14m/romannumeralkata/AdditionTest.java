e32b83861839ef512cc41f8830de48208e208e9d
Moss Collum <moss.collum@gmail.com>
2013-04-04 13:29:45 -0400

    failing test: use format from first part of addition

--- test/net/m14m/romannumeralkata/AdditionTest.java
+++ test/net/m14m/romannumeralkata/AdditionTest.java
@@ -10,15 +10,26 @@ public class AdditionTest {
         assertEquals(4, addition.getValue());
     }
 
+    @Test public void shouldUseTheFormatOfTheFirstExpressionInTheAddition() {
+        Addition romanAddition = new Addition(Constant.roman(1), Constant.arabic(3));
+        Addition arabicAddition = new Addition(Constant.arabic(1), Constant.arabic(3));
+        assertEquals("V", romanAddition.getFormatter().format(5));
+        assertEquals("5", arabicAddition.getFormatter().format(5));
+    }
+
     public static class Constant implements Expression {
         private final int value;
-        private ArabicNumeralFormatter formatter;
+        private final Formatter formatter;
 
         public static Expression arabic(final int value) {
             return new Constant(value, new ArabicNumeralFormatter());
         }
 
-        public Constant(int value, ArabicNumeralFormatter formatter) {
+        public static Expression roman(int value) {
+            return new Constant(value, new RomanNumeralFormatter());
+        }
+
+        public Constant(int value, Formatter formatter) {
             this.value = value;
             this.formatter = formatter;
         }
