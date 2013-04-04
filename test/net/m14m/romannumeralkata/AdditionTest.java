a80c8f4ab1f7861fca57f445efd08290ee37e9f9
Moss Collum <moss.collum@gmail.com>
2013-04-04 13:26:46 -0400

    refactor: constant expression

--- test/net/m14m/romannumeralkata/AdditionTest.java
+++ test/net/m14m/romannumeralkata/AdditionTest.java
@@ -6,19 +6,29 @@ import static org.junit.Assert.assertEquals;
 
 public class AdditionTest {
     @Test public void shouldAddUpNumbersCorrectly() {
-        Addition addition = new Addition(arabicConstant(1), arabicConstant(3));
+        Addition addition = new Addition(Constant.arabic(1), Constant.arabic(3));
         assertEquals(4, addition.getValue());
     }
 
-    private Expression arabicConstant(final int value) {
-        return new Expression() {
-            public int getValue() {
-                return value;
-            }
+    public static class Constant implements Expression {
+        private final int value;
+        private ArabicNumeralFormatter formatter;
 
-            public Formatter getFormatter() {
-                return new ArabicNumeralFormatter();
-            }
-        };
+        public static Expression arabic(final int value) {
+            return new Constant(value, new ArabicNumeralFormatter());
+        }
+
+        public Constant(int value, ArabicNumeralFormatter formatter) {
+            this.value = value;
+            this.formatter = formatter;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Formatter getFormatter() {
+            return formatter;
+        }
     }
 }
